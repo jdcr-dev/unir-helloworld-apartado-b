661ac84079bc2a6dc56da4979578e5f8c132a71e
daniel castro <danielcastro@opercanarios.com>
2024-05-30 19:49:12 +0100

    Vuelvo a los originales de anietto

--- app/calc.py
+++ app/calc.py
@@ -1,13 +1,11 @@
 import app
-from typing import Final
+
 
 class InvalidPermissions(Exception):
     pass
 
 
 class Calculator:
-    SQRT_VALUE: Final[float] = 0.5
-
     def add(self, x, y):
         self.check_types(x, y)
         return x + y
@@ -23,7 +21,7 @@ class Calculator:
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise TypeError("Division by zero is not possible")
+            raise                                                TypeError("Division by zero is not possible")
 
         return x / y
 
@@ -31,14 +29,10 @@ class Calculator:
         self.check_types(x, y)
         return x ** y
 
-    def sqrt(self, x):
-        self.check_types(x)
-        return self.power(x, self.SQRT_VALUE)
+    def check_types(self, x, y):
+        if not isinstance(x, (int, float)) or                                                not isinstance(y, (int, float)):
+            raise TypeError("Parameters must be numbers")
 
-    def check_types(self, *args):        
-        for arg in args:            
-            if not isinstance(arg, (int, float)):
-                raise TypeError("Parameters must be numbers")
 
 if __name__ == "__main__":  # pragma: no cover
     calc = Calculator()
