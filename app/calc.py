69f7d32f3c2738a006e5f68f38795fa904630232
daniel castro <danielcastro@opercanarios.com>
2024-05-31 23:10:39 +0100

    wip

--- app/calc.py
+++ app/calc.py
@@ -28,6 +28,10 @@ class Calculator:
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
+    
+    def sqrt(self, x):
+        self.check_types(x)
+        return self.power(x, self.SQRT_VALUE)
 
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or                                                not isinstance(y, (int, float)):
